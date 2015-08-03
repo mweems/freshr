345bb4d284531d0d52cbdda6a53459bffdf7b3f4
Matt Weems <mj_weems@yahoo.com>
2015-08-03 19:36:46 +0100

    adding create post page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
 if __name__ == "__main__":
     app.run()
