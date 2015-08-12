8dd082ef97d31851e974806015af1d44ed7724a6
Matt Weems <mj_weems@yahoo.com>
2015-08-12 17:48:57 +0100

    add home page html

--- freshr/functional_tests.py
+++ freshr/functional_tests.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewFishermanTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -12,6 +14,12 @@ class NewFishermanTest(unittest.TestCase):
 	def test_can_choose_to_sell_or_buy_fish(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('Freshr', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Welcome to Freshr', header_text)
+
+		sell_button = self.browser.find_element_by_id('sell_fish')
+
+		sell_button.send_keys(Keys.ENTER)
 
 		self.fail('Finish the test!')
 
