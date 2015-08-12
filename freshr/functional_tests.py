a6cd52a4372b0bce5c006cbed13de8a8f812da1f
Matt Weems <mj_weems@yahoo.com>
2015-08-12 17:24:28 +0100

    adding initial infrastructure

--- freshr/functional_tests.py
+++ freshr/functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewFishermanTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_choose_to_sell_or_buy_fish(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Freshr', self.browser.title)
+
+		self.fail('Finish the test!')
+
+	
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
