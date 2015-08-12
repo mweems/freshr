ada6ed84a7956d2885b2542858607ab8dda99df9
Matt Weems <mj_weems@yahoo.com>
2015-08-12 17:36:11 +0100

    adding first unit tests and basic responses

--- freshr/feed/tests.py
+++ freshr/feed/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from feed.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-	def test_bad_maths(self):
-		self.assertEqual(1+1, 3)
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>Freshr</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
