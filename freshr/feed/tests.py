abf2beedebddd389640e3d802d69d4b2fb553930
Matt Weems <mj_weems@yahoo.com>
2015-08-13 12:29:11 +0100

    Model for list items and associated migration

--- freshr/feed/tests.py
+++ freshr/feed/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from feed.views import home_page, create_page
+from feed.models import Item
 
 class HomePageTest(TestCase):
 
@@ -36,4 +37,23 @@ class CreatePostPageTest(TestCase):
 		response = create_page(request)
 
 		self.assertIn('A new item', response.content.decode())
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
 		
