f5502d6434dc060f464b40971ce1b52be5dd21cc
Matt Weems <mj_weems@yahoo.com>
2015-08-17 15:14:34 +0100

    refactor list view to handle new item posts

--- freshr/feed/tests/test_views.py
+++ freshr/feed/tests/test_views.py
@@ -59,6 +59,32 @@ class ListViewTest(TestCase):
 		correct_list = List.objects.create()
 		response = self.client.get('/feed/%d/' % correct_list.id)
 		self.assertEqual(response.context['list'], correct_list)
+
+	def test_can_save_a_post_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post(
+			'/feed/%d/' % correct_list.id,
+			data={'item_text': 'A new item to an existing list'}
+		)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item to an existing list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_post_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post(
+			'/feed/%d/' % correct_list.id,
+			data={'item_text': 'A new item to an existing list'}
+		)
+
+		self.assertRedirects(response, '/feed/%d/' % correct_list.id)
+
 		
 class NewListTest(TestCase):
 
@@ -93,33 +119,3 @@ class NewListTest(TestCase):
 		)
 		self.assertEqual(List.objects.count(), 0)
 		self.assertEqual(Item.objects.count(), 0)
-
-
-class NewItemTest(TestCase):
-
-	def test_can_save_a_post_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-
-		self.client.post(
-			'/feed/%d/add_item' % correct_list.id,
-			data={'item_text': 'A new item to an existing list'}
-		)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new item to an existing list')
-		self.assertEqual(new_item.list, correct_list)
-
-	def test_redirects_to_list_view(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-
-		response = self.client.post(
-			'/feed/%d/add_item' % correct_list.id,
-			data={'item_text': 'A new item to an existing list'}
-		)
-
-		self.assertRedirects(response, '/feed/%d/' % correct_list.id)
-
-		
--- freshr/feed/urls.py
+++ freshr/feed/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
 	url(r'^create$', views.create_page, name='create'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
-    url(r'^(\d+)/add_item$', views.add_item, name='add_item')
 ]
