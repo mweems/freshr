60553e78a20f4f29940ad005ea4bd42abec8fecc
Matt Weems <mj_weems@yahoo.com>
2015-08-17 12:28:30 +0100

    add classmethods for eventual deployment to live domain

--- freshr/functional_tests/tests.py
+++ freshr/functional_tests/tests.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 import time
 
 class NewFishermanTest(StaticLiveServerTestCase):
+
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -24,7 +40,7 @@ class NewFishermanTest(StaticLiveServerTestCase):
 		self.assertIn('Create Post', self.browser.title)
 
 	def test_can_choose_to_sell_fish(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.assertIn('Freshr', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('Welcome to Freshr', header_text)
@@ -38,7 +54,7 @@ class NewFishermanTest(StaticLiveServerTestCase):
 		self.assertRegex(create_post_url, '/feed/create')
 
 	def test_can_create_a_post_and_retrieve_it_later(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.go_to_sell_page()
 		header_text = self.browser.find_element_by_tag_name('h2').text
 		
@@ -63,7 +79,7 @@ class NewFishermanTest(StaticLiveServerTestCase):
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.go_to_sell_page()
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('50lbs Tuna', page_text)
